6de758e60328464c76660ac6d0fc6428498f4cda
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-02-16 23:08:30 +0200

    app jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./sections/Home";
+import CV from "./sections/CV";
+import Contacts from "./sections/Contact";
+
+function App() {
+  return (
+    <main>
+      <Home />
+      <CV />
+      <Contacts />
+    </main>
+  );
+}
+
+export default App;
+

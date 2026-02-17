0ba900ab5d10ac5840bfe07006dd01fe0645cc9f
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-02-17 20:27:25 +0200

    import projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Home from "./sections/Home";
 import CV from "./sections/CV";
 import Contacts from "./sections/Contact";
 import StackLevel from "./sections/StackLevel";
+import Projects from "./sections/Projects";
 
 function App() {
   return (
     <main>
       <Home />
       <CV />
+      <Projects/>
       <StackLevel/>
       <Contacts />
     </main>

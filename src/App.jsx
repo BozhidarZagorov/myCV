d969c74b6832dfb7cf3f7e2787c0533b91843a42
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-02-17 20:27:18 +0200

    import stack levels

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Home from "./sections/Home";
 import CV from "./sections/CV";
 import Contacts from "./sections/Contact";
+import StackLevel from "./sections/StackLevel";
 
 function App() {
   return (
     <main>
       <Home />
       <CV />
+      <StackLevel/>
       <Contacts />
     </main>
   );

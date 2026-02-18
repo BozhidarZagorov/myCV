cdeaed6740fe9dc6de5530f24fc51aacd2c4c037
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-02-18 22:35:46 +0200

    remove imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import Home from "./sections/Home";
 import CV from "./sections/CV";
 import Contacts from "./sections/Contact";
-import StackLevel from "./sections/StackLevel";
-import Projects from "./sections/Projects";
 
 function App() {
   return (
     <main>
       <Home />
       <CV />
-      <Projects/>
-      <StackLevel/>
       <Contacts />
     </main>
   );

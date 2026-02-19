e257684c6d9e36981546650f2c38d8d3ffa3bb2f
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-02-19 21:59:04 +0200

    add other skills

--- src/sections/CV.jsx
+++ src/sections/CV.jsx
@@ -298,6 +298,7 @@ export default function CV() {
 
           <div className="skills-list">
             <span>Vite</span>
+            <span>Postman</span>
             <span>Supabase</span>
             <span>Vercel</span>
             <span>Cloudinary</span>
@@ -315,6 +316,7 @@ export default function CV() {
             <span>PostgreSQL</span>
             <span>Python</span>
             <span>C++</span>
+            <span>C#</span>
           </div>
         </div>
 
